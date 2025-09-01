64caca0d03fef2e2cbd43aa711ab6a40080ca00a
themaverick27 <aniweshkumar04@gmail.com>
2025-09-02 02:21:31 +0530

    first commit

---
+++ 01_basics/chai.py
@@ -0,0 +1,3 @@
+from hello_world import func
+
+func("Chai aur code")
---
+++ 01_basics/hello_world.py
@@ -0,0 +1,11 @@
+print("hello world!")
+
+def func(n):
+    print(n)
+
+func(4)
+func("Apple")
+
+chai_one = "Lemon Tea"
+chai_two = "Masala Tea"
+chai_three = "Ginger Tea"
